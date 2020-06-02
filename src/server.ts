40cc5ffdd62a103254001d3f0e11bc56985c6639
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-02 11:10:49 -0300

    config conexão com banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
-
-  response.json([
-    'Gustavo',
-    'Diego',
-    'Teló'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
