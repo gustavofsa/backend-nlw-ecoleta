bc03458406e5c99901f16d9bd830ec1dfcf68e76
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-03 23:49:12 -0300

    corrigindo seed

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -7,6 +7,6 @@ export async function seed(knex: Knex) {
     { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-    { title: 'Óleo de Cozinha', image: 'lampadas.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
   ]);
 }
